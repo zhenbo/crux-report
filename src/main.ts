c0c17b8a6d1f318df4a7d0161148b9906368084a
Zhenbo Shi <zhenbo.shi@eventbrite.com>
2023-04-20 00:07:30 -0400

    feat(crux-api): fetch data via api

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,79 @@
-#!/usr/bin/env node
+import axios, { AxiosRequestConfig } from 'axios'
+import { createObjectCsvWriter } from 'csv-writer'
 
-/**
- * This is a sample HTTP server.
- * Replace this with your implementation.
- */
+const API_KEY = 'AIzaSyC_WmfG1tPj-2AjAY3rqYIx7S4d6KR5Zf0'
+const API_URL = 'https://chromeuxreport.googleapis.com/v1/records:queryHistoryRecord'
+const CSV_FILE_NAME = './dist/crux-data.csv'
+const FORM_FACTOR = 'DESKTOP'
 
-import 'dotenv/config'
-import { createServer, IncomingMessage, ServerResponse } from 'http'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-import { Config } from './config.js'
+// Set the URLs you want to fetch CrUX data for
+const urls: string[] = [
+  'https://www.eventbrite.com',
+  'https://www.eventbrite.com/signin/signup',
+  'https://www.eventbrite.com/d/ca--san-francisco/events/',
+  'https://www.eventbrite.com/contact/',
+]
 
-const nodePath = resolve(process.argv[1])
-const modulePath = resolve(fileURLToPath(import.meta.url))
-const isCLI = nodePath === modulePath
+// Set the CrUX metrics you want to retrieve
+const cruxMetrics: string[] = [
+  'largest_contentful_paint',
+  'first_input_delay',
+  'cumulative_layout_shift',
+  'first_contentful_paint',
+  'experimental_interaction_to_next_paint',
+  'experimental_time_to_first_byte',
+]
 
-export default function main(port: number = Config.port) {
-  const requestListener = (request: IncomingMessage, response: ServerResponse) => {
-    response.setHeader('content-type', 'text/plain;charset=utf8')
-    response.writeHead(200, 'OK')
-    response.end('Olá, Hola, Hello!')
+// Set the headers for the CSV file
+const csvHeaders = [{ id: 'url', title: 'URL' }]
+cruxMetrics.forEach(metric => {
+  csvHeaders.push({ id: metric, title: metric })
+})
+
+// Create a CSV writer to write the data to a file
+const csvWriterInstance = createObjectCsvWriter({
+  path: CSV_FILE_NAME,
+  header: csvHeaders,
+})
+
+// Function to fetch CrUX data and write to CSV
+async function fetchCrUXData(): Promise<void> {
+  // Loop through each URL and fetch CrUX data
+  for (let i = 0; i < urls.length; i++) {
+    try {
+      // Construct the API request
+      const requestBody = {
+        formFactor: FORM_FACTOR,
+        url: urls[i],
+        metrics: cruxMetrics,
+      }
+
+      const requestConfig: AxiosRequestConfig = {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        params: {
+          key: API_KEY,
+        },
+      }
+
+      // Make the API request
+      const response = await axios.post(API_URL, requestBody, requestConfig)
+
+      // Write the data to the CSV file
+      const data = [{ url: urls[i] }]
+      await csvWriterInstance.writeRecords(data)
+      console.log(`Data fetched and written to CSV for URL: ${urls[i]} ${response.data}`)
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(`Error fetching data for URL: ${urls[i]} - ${error.message}`)
+      }
+    }
+    // Delay for the rate limit before fetching data for the next URL
+    await new Promise(resolve => setTimeout(resolve, 60000 / 100)) // 60000 ms = 1 minute
   }
-
-  const server = createServer(requestListener)
-
-  if (isCLI) {
-    server.listen(port)
-    // eslint-disable-next-line no-console
-    console.log(`Listening on port: ${port}`)
-  }
-
-  return server
 }
 
-if (isCLI) {
-  main()
-}
+// Start fetching CrUX data
+fetchCrUXData()
